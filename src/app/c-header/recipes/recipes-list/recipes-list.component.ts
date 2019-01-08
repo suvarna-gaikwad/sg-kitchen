41f328b10cbc21fced5db9583e52dbcb5ceeebcd
suvarna-gaikwad <suvarna.gaikwad12@gmail.com>
2019-01-08 10:22:41 +0530

    Added recipe details, ingredient model and shopping list

--- src/app/c-header/recipes/recipes-list/recipes-list.component.ts
+++ src/app/c-header/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+
+  recipeList : Recipe[] = [
+    new Recipe('Puranpoli','sweet indian bread','https://upload.wikimedia.org/wikipedia/commons/7/71/Obbattu.jpg'),
+    new Recipe('Paneer Springroll','fried roll stuffed with paneer','https://cdn.pixabay.com/photo/2017/07/25/00/23/spring-rolls-2536526_1280.jpg')
+  ];
 
   constructor() { }
 
--- src/app/c-header/shooping-list/shooping-list.component.ts
+++ src/app/c-header/shooping-list/shooping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shooping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shooping-list.component.css']
 })
 export class ShoopingListComponent implements OnInit {
+
+  ingredients : Ingredient[] = [
+    new Ingredient('cardomom',5),
+    new Ingredient('cinamon',2)
+  ]
 
   constructor() { }
 
